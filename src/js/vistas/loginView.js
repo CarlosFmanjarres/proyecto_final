7d88f01c327cda4490863cc4da609a9871ef3019
carlos_manjarres <manjarresc63@gmail.com>
2024-08-17 09:58:58 -0500

    se realizo la vista de el login

---
+++ src/js/vistas/loginView.js
@@ -0,0 +1,10 @@
+import { AuthController } from '../controllers/authController.js';
+
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+  event.preventDefault();
+  
+  const email = document.getElementById('loginEmail').value;
+  const password = document.getElementById('loginPassword').value;
+
+  AuthController.login(email, password);
+});
